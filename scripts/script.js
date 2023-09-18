7abe20673b93757adb5a5791364bf91a6c67ec7e
mousse <guillaume.bergerot@etu.unicaen.fr>
2023-09-18 16:44:21 +0200

    updated js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-const timer = document.getElementById("timer")
-const bottle = document.getElementById("bottle")
+const timer = document.getElementById("timer");
+const bottle = document.getElementById("bottle");
+const play = document.getElementById("play");
 let currentInterval;
 
 let isGoing = false
@@ -8,20 +9,41 @@ let time = 25*60;
 
 timer.textContent = `${Math.floor(time/60)}:${(time%60)<10?"0":""}${time%60}`;
 
-function updateTimer()
+function updateDisplay()
+{
+    timer.textContent = `${Math.floor(time/60)}:${(time%60)<10?"0":""}${time%60}`;
+    if (isGoing)
+    {
+        
+    }
+}
+
+function update()
 {
     time -=1;
+    updateDisplay();
+}
+
+function reset()
+{
+    isGoing = false;
+        play.classList.remove("fa-xmark");
+        play.classList.add("fa-xplay");
+        clearInterval(currentInterval);
+        time = 25*60;
 }
 
 function buttonPressed() {
     if (isGoing)
     {
-        isGoing = false;
-        clearInterval(currentInterval);
+        reset();
+        updateDisplay();
     }
     else
     {
         isGoing = true;
-        currentInterval = setInterval(updateTimer(),1000);
+        play.classList.remove("fa-xplay");
+        play.classList.add("fa-xmark");
+        currentInterval = setInterval(() => update(),1000);
     }
 }
